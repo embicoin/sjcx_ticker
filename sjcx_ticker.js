f612c3d77feafd0dfe0f4728b0a9de53ad464184
dame <dame@dame>
2015-01-21 16:13:31 +0900

    1st

---
+++ sjcx_ticker.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var app = express();
+
+var request=require('request');
+var n=3;
+app.get('/', function (req, res) {
+    var result=[];
+    master(res,result);
+    melotic(res,result);
+    poloniex(res,result);
+});
+
+function master(res,r){
+    var result={};
+    result.name="masterXchange";
+    var dt = parseInt(new Date().getTime()/1000);
+    dt-=24*60*60; 
+    var url="https://masterxchange.com/api/v2/trades.php?timestamp="+dt+"&currency=xcpsjcx";
+    request(url,function(error,response,body){
+        try{
+            if (!error && response.statusCode == 200) {
+                var json=JSON.parse(body);
+                if(json.length>0){
+                    value=json[0]["price"];
+                    var volume=0;
+
+                    for(var i=0;i<json.length;i++){
+                        volume+=parseFloat(json[i]["amount"]);
+                    }
+
+                    result.value=value;
+                    result.volume=volume;
+                }else{
+                    result.value=-1;
+                    result.volume=0;
+                }
+            } else {
+                result.value=-1;
+                result.volume=0;
+            }
+        }catch(e){
+            result.value=-1;
+            result.volume=0;
+        }finally{
+            r.push(result);
+            n--;
+            send(res,r);
+        }
+    });
+}
+
+function melotic(res,r){
+    var result={};
+    var url="https://www.melotic.com/api/markets";
+    request(url,function(error,response,body){
+        try{
+            if (!error && response.statusCode == 200) {
+                var json=JSON.parse(body);
+                var value=json["sjcx-btc"]["latest_price"];
+                var volume=json["sjcx-btc"]["volume"];
+                result=({name:"melotic",value:value,volume:volume});
+            } else {
+                result=({name:"melotic",value:-1,volume:0});
+            }
+        }catch(e){
+            result=({name:"melotic",value:-1,volume:0});
+        }finally{
+            r.push(result);
+            n--;
+            send(res,r);
+        }
+    });
+}
+
+function poloniex(res,r){
+    var result={};
+    var url="https://poloniex.com/public?command=returnTicker";
+    request(url,function(error,response,body){
+        try{
+            if (!error && response.statusCode == 200) {
+                var json=JSON.parse(body);
+                var value=json["BTC_SJCX"]["last"];
+                var volume=json["BTC_SJCX"]["baseVolume"];
+                result=({name:"poloiex",value:value,volume:volume});
+            } else {
+                result=({name:"poloiex",value:-1,volume:0});
+            }
+        }catch(e){
+            result-({name:"poloiex",value:-1,volume:0});
+        }finally{
+            r.push(result);
+            n--;
+            send(res,r);
+        }
+    });
+}
+
+function send(res,result){
+    if(n==0){
+        res.json(result);
+    }
+}
+
+app.listen(process.env.PORT || 3000);
